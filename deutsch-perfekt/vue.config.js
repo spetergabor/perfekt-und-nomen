9fd99c6db26dca75566d88c8bb838a9ec516e3b9
spetergabor <spetergabor@gmail.com>
2026-05-12 21:35:59 +0200

    force refresh: updated audio path with cache busting

--- deutsch-perfekt/vue.config.js
+++ deutsch-perfekt/vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/perfekt-und-nomen/' : '/'
+  // A GitHub Pages-hez használt fix útvonal helyett relatív útvonalat használunk
+  publicPath: './',
+  
+  // Opcionális: kikapcsolhatod a hash-elt fájlneveket, ha fix neveket akarsz, 
+  // de a korábban írt PHP glob() függvényünk a hash-elt nevekkel is elboldogul.
+  filenameHashing: true 
 }
